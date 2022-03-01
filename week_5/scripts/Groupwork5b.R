af2079b7152d2bb35beea9d60176c7849b8827d3
MargaritaH34 <margarita.alfarohernandez.776@my.csun.edu>
2022-03-01 11:36:59 -0800

    week_5

--- week_5/scripts/Groupwork5b.R
+++ week_5/scripts/Groupwork5b.R
@@ -17,28 +17,28 @@ DepthData <- read_csv(here("week_5", "data", "DepthData.csv"))
 CD <- condData %>%
   mutate(CD = mdy_hms(depth)) %>% # adds new colums
   drop_na() %>%
-  mutate(Datetime = round_date(CD, "10 sec")) %>%
-  mutate(date = Datetime) %>%
+  mutate(Datetime = round_date(CD, "10 sec")) %>% # add "DateTime" to create new column
+  mutate(date = Datetime) %>% #creates new column names "date"
   select(date, TempInSitu, SalinityInSitu_1pCal) %>%
-  inner_join(DepthData) %>%
-  mutate(hour = hour(date)) %>%
-  mutate(minute = minute(date)) %>%
+  inner_join(DepthData) %>% # inner joins, joins both data set
+  mutate(hour = hour(date)) %>% # creates a column name "hou"
+  mutate(minute = minute(date)) %>% #creates column named "minute
   mutate(day = day(date)) %>%
-  unite(col = "HourMin",
+  unite(col = "HourMin", #unites the hour and minute column into a column named "HourMin"
         c(hour, minute),
-        sep = ":",
+        sep = ":", #separates the hour and minute by ":"
         remove = TRUE) %>%
   group_by(HourMin) %>%
-  summarise(MeanDepth = mean(Depth, na.rm = TRUE),
+  summarise(MeanDepth = mean(Depth, na.rm = TRUE), #Depth in mts
             MeanSalinity = mean(SalinityInSitu_1pCal, na.rm =TRUE),
             MeanTemp = mean(TempInSitu, na.rm = TRUE),
             MeanDate = mean(date, na.rm = TRUE))
 
 CD %>%
-  ggplot(aes(x= MeanDate, y= MeanTemp)) +
-  geom_line(color = "red")+
+  ggplot(aes(x= MeanDate, y= MeanTemp)) + #plotting MeanDate vs MeanTemp
+  geom_line(color = "red")+ #line graph
   labs(x= "TIME", y= "Temperature (C)", title = "Average Temperature Over time")+
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5)) # centers the title
 
 ggsave(here("week_5", "output", "Conductivity_Depth.png"))
 
